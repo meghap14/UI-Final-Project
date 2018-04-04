1a7e6db5562eaa9451eab54b4dc21e967c28ef97
mrmintz <mrmintz@umich.edu>
2018-04-04 11:57:34 -0400

    made accounts database

--- UI_project/server/main.js
+++ UI_project/server/main.js
@@ -1,5 +1,15 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+  //creates server side Accounts database (so info persists on refresh)
+  Accounts = new Mongo.Collection('accounts');
 });
+
+Meteor.methods({
+	delete_account : function(name) {
+		Accounts.remove({ username : name });
+	}
+})
