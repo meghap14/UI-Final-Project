17c861a01db0514738d88bdc85b024acd57ef00f
mrmintz <mrmintz@umich.edu>
2018-04-16 21:14:43 -0400

    trying to restore

--- UI_project/client/main.js
+++ UI_project/client/main.js
@@ -1,7 +1,7 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import './main.html';
+//import './main.html';
 //import THREE from 'three';
 var THREE = require('three');
 var OrbitControls = require('three-orbit-controls')(THREE);
